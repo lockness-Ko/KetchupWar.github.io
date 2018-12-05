db2efff3fa6cb1b88d50db2adc6cfabc2026b073
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-05 12:01:50 +1100

    Create environment.js

---
+++ environment.js
@@ -0,0 +1,4 @@
+import './3d.js';
+//There kind of needs to be a cube function for this to work but random worlds can be generted by having random cubes in random places
+//cube(10, 10, 10, 10);
+//TODO put that function in 3d.js cos 3d.js is the 3d engine
